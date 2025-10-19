5ad7ea931332b1b5661716adc3efddce24a793ba
Tom Whittaker <tom@tomw.org>
2025-10-19 16:06:40 -0500

    updated alarm sounds

--- app/src/main/java/org/tomw/cathnow/data/SoundOption.kt
+++ app/src/main/java/org/tomw/cathnow/data/SoundOption.kt
@@ -3,10 +3,10 @@ package org.tomw.cathnow.data
 import org.tomw.cathnow.R
 
 enum class SoundOption(val displayName: String, val soundId: Int? = null) {
-    ALARM_1("Alarm 1", R.raw.alarm1),
-    ALARM_2("Alarm 2", R.raw.alarm2),
-    ALARM_3("Alarm 3", R.raw.alarm3),
-    ALARM_4("Alarm 4", R.raw.alarm4);
+    ALARM_1("The Tick", R.raw.alarm1),
+    ALARM_2("Alert Sound", R.raw.alarm2),
+    ALARM_3("Klaxon", R.raw.alarm3),
+    ALARM_4("Flush", R.raw.alarm4);
 
     companion object {
         fun fromDisplayName(name: String): SoundOption {
