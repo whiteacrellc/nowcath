983d51fec66bbb310cf09d71c5cfc6423d39ef08
Tom Whittaker <tom@tomw.org>
2025-10-19 18:05:08 -0500

    Updated round icons and change menu to hamburger style.

--- app/src/main/java/org/tomw/cathnow/data/SoundOption.kt
+++ app/src/main/java/org/tomw/cathnow/data/SoundOption.kt
@@ -6,7 +6,7 @@ enum class SoundOption(val displayName: String, val soundId: Int? = null) {
     ALARM_1("The Tick", R.raw.alarm1),
     ALARM_2("Alert Sound", R.raw.alarm2),
     ALARM_3("Klaxon", R.raw.alarm3),
-    ALARM_4("Flush", R.raw.alarm4);
+    ALARM_4("Retro Alarm", R.raw.alarm4);
 
     companion object {
         fun fromDisplayName(name: String): SoundOption {
